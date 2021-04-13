5a8b3180b57805ae67ef847fc3e710c742b18505
Mirajus Salehin <mirajussalehin@gmail.com>
2021-04-13 17:24:32 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,78 @@
+import requests
+import json
+import inspect
+
+from API import APIinfo
+
+
+API_URL = "http://api.openweathermap.org/data/2.5/weather?q=" + APIinfo["query"]["city_name"] + "," + APIinfo["query"]["country_code"] + "&units=" + APIinfo["units"] + "&appid=" + APIinfo["API_KEY"]
+
+
+weatherResponseRaw = requests.get(API_URL)
+weatherResponse = weatherResponseRaw.json()
+
+
+class Weather:
+    def __init__(self):
+        self.weather = weatherResponse["weather"][0]
+        self.main = weatherResponse["main"]
+        self.wind = weatherResponse["wind"]
+        self.sys = weatherResponse["sys"]
+        self.coord = weatherResponse["coord"]
+
+    def getCoord(self, option: str):
+        if option in ["lat", "lon"]:
+            return self.coord[option]
+        else:
+            return "INVALID OPTON"
+
+    def getWeather(self, option: str):
+        '''
+        returns the main weather information
+        @param option type str
+        options can be main,description,icon,id
+        '''
+
+        if option in ["id", "main", "description", "ison"]:
+            return self.weather[option]
+        else:
+            return "INVALID OPTION"
+
+    def getMain(self, option: str):
+        if option in ["temp", "feels_like", "temp_min", "temp_max", "pressure", "humidity", "sea_level", "grnd_level"]:
+            return self.main[option]
+        else:
+            return "INVALID OPTION"
+
+    def getVisibility(self):
+        return weatherResponse["visibility"]
+
+    def getWind(self, option: str):
+        if option in ["speed", "deg"]:
+            return self.wind[option]
+        else:
+            return "INVALID OPTION"
+
+    def getSys(self, option: str):
+        if option in ["country", "sunrise", "sunset"]:
+            return self.sys[option]
+        else:
+            return "INVALID OPTON"
+
+
+w = Weather()
+
+info = inspect.cleandoc(f'''
+        Current Temparature : {w.getMain("temp")}
+        Feels like : {w.getMain("feels_like")}
+'''
+                        )
+
+
+def main():
+    if weatherResponse["cod"] == 200:
+        print(info)
+
+
+if __name__ == "__main__":
+    main()
